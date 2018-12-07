758cfe31ef68dda845ff11af361e9c95a9b993b3
Abdelhak Akid <abdelhakakid@gmail.com>
2018-12-07 16:51:24 +0100

    commit suivant

--- index.php
+++ index.php
@@ -1,45 +1,32 @@
 <?php
-$filename = "data.json" ;
+$filename = "data.txt" ;
 $data_json = file_get_contents($filename) ;
 $data = json_decode($data_json) ;
 $date_update = filemtime($filename);
 
 $bargraph_height = 161 + $data->temperature * 4;
 $bargraph_top = 315 - $data->temperature * 4;
-?>
+ ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-<meta charset="utf-8">
-<meta http-equiv="refresh" content="5">
-<link rel="stylesheet" type="text/css" href="main.css"></link>
-	<title></title>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="1">
+    <title>DTH11 NodeMcu</title>
+    <link rel="stylesheet" href="main.css">
 </head>
 <body>
+<h1>Température</h1>
 
-
-<h1 >Température</h1>
-
-<?php
-
-$file = "data.txt";
-$texte = file_get_contents($file);
-$data = json_decode($texte);
-$bargraph_height = 160+ $data->temperature * 4;
-$bargraph_top = 315+$data->temperature * 4*-1;
-
-?>
-<p>il fait <?php echo $data->temperature; ?>° avec <?php echo $data->humidite ?>% d'humidité
- Le <?php echo date("d/m/Y", $date_update) ; ?>
-    à <?php echo date("H:i:s", $date_update) ; ?>
-    </p>
+<p>
+  Il fait <?php echo $data->temperature ; ?>°C avec <?php echo $data->humidite ; ?>% d'humidité.</br>
+  Le <?php echo date("d/m/Y", $date_update) ; ?>
+  à <?php echo date("H:i:s", $date_update) ; ?>.
+</p>
 
 <div id="thermometer">
-
-  <div id="bargraph" style=<?php echo "\"height:".$bargraph_height."px; top:".$bargraph_top."px;\""?>
-
-	>
-  </div>
+  <div id="bargraph" style="height: <?php echo $bargraph_height ; ?>px; top: <?php echo $bargraph_top ; ?>px;"></div>
 </div>
 
 </body>
---
+++ store_temp.php
@@ -0,0 +1,27 @@
+<?php
+
+$filename = "data.txt" ;
+
+$data_json = file_get_contents("php://input") ;
+$data = json_decode($data_json);
+
+if(! $data) {
+
+	http_response_code(415);
+
+    exit();
+
+}
+elseif(! $data->temperature || ! $data->humidite){
+http_response_code(400);
+exit();
+
+}
+
+    $op = file_put_contents($filename, $data_json) ;
+    if (! $op) {
+
+        echo "store error" ;
+    }
+
+?>
